91393a3a119a8da7d406cb431bfd7a03f8b3b7f6
Evohunt <evohuntmaster@gmail.com>
2018-09-25 00:11:21 +0300

    Updated GUI (Home + Buttons)

--- src/Hotel/Main.java
+++ src/Hotel/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(new Scene(root, 900, 600));
         primaryStage.show();
+
     }
 
 
--- src/Hotel/MainController.java
+++ src/Hotel/MainController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Border;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
 import javax.swing.*;
